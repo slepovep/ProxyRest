5c24679ecf22ea4e36acaac9d56feb5253cf8d36
slepovep <slepov.ep@gmail.com>
2021-02-09 18:49:50 +0300

    no result

--- ProxyRest/Controllers/ProxyRestController.cs
+++ ProxyRest/Controllers/ProxyRestController.cs
@@ -5,6 +5,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using System.Web;
+using System.Text;
+
+
+//using Microsoft.AspNetCore.
+
 
 namespace ProxyRest.Controllers
 {
@@ -41,12 +48,35 @@ namespace ProxyRest.Controllers
 		}
 
 		[HttpPost]
-		public string Post()
+		public static async Task<string> Post(/*HttpContent content*/)
 		{
+			var httpRequestMessage = new HttpRequestMessage();
+			HttpClient client = new HttpClient();
+			HttpContent content = new StringContent("pJsonContent", Encoding.UTF8, "application/json");
+			httpRequestMessage.Content = content;
+
+			//var content = "new FormUrlEncodedContent(values)";
+
+			var response = await client.PostAsync("http://www.example.com/recepticle.aspx", content);
+
+			var responseString = await response.Content.ReadAsStringAsync();
+
+			/*var req = context.HttpContext.Request;
+			var request = context.HttpContext.Request;
+			var stream = new StreamReader(request.Body);
+			var body = stream.ReadToEnd();
+
+			HttpContext.Request.
+
+			HttpContent requestContent = Request.Content;
+			string jsonContent = requestContent.ReadAsStringAsync().Result;
+
 			//находим входящий GET-запрос
 			string query = this.Request.QueryString.ToString();
 			//Task<string> content;
 			HttpContent content;
+
+			var httpContent = this.Request.GetTypeContent;
 			content = this.Request.Query.
 
 			if (string.IsNullOrEmpty(query))
@@ -68,11 +98,22 @@ namespace ProxyRest.Controllers
 			catch
 			{
 				return "Ошибка запроса:" + "\n" + address_url;
-			};
+			};*/
+
+			//HttpClient client = new HttpClient();
+			//string query = this.Request.QueryString.ToString();
+
+			//HttpContent content = new StringContent("this.Request.QueryString.ToString()");
+			//HttpResponseMessage response = await client.PostAsync("url", content).ConfigureAwait(false);
+			//return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+			//string result = await this.Request.Content.ReadAsStringAsync();
+			//return responseString;
+
+			return await client.SendAsync(httpRequestMessage).ToString();
+
+
 
 		}
-
-
-
 	}
 }
