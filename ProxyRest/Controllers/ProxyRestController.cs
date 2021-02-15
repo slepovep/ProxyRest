93a24f68d66e92f73122c5aff66e6f8313892e89
slepovep <slepov.ep@gmail.com>
2021-02-16 01:04:51 +0300

    ошибка отправки заголовка Request headers must contain only ASCII characters.

--- ProxyRest/Controllers/ProxyRestController.cs
+++ ProxyRest/Controllers/ProxyRestController.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.IO;
 using Microsoft.Extensions.Configuration;
 using System;
-
-
+using System.Web;
 
 namespace ProxyRest.Controllers
 {
@@ -53,7 +52,9 @@ namespace ProxyRest.Controllers
 		[HttpPost]
 		public async Task<string> Post()
 		{
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 			string body;
+			string headerstr = "";
 			HttpClient client = new HttpClient();
 			var httpRequestMessage = new HttpRequestMessage();
 			var Urllocalgit = Configuration["UrlLocalGit"];
@@ -74,12 +75,36 @@ namespace ProxyRest.Controllers
 			{ 
 				foreach (var header in Request.Headers)
 				{
-					if (!header.Key.ToString().Contains("Content"))
+					/*if (header.Key.ToString().Contains("CommitMes"))  
 					{
-						client.DefaultRequestHeaders.Add(header.Key.ToString(), header.Value.ToString());
+
+						string commitmes = HttpUtility.HtmlEncode(Request.Headers["CommitMes"].ToString()); //обработка русских символов
+
+						Encoding utf8 = Encoding.GetEncoding("UTF-8");
+						Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+
+						byte[] utf8Bytes = win1251.GetBytes(commitmes);
+						byte[] win1251Bytes = Encoding.Convert(utf8, win1251, utf8Bytes);
+
+						commitmes = win1251.GetString(win1251Bytes);
+
+						client.DefaultRequestHeaders.TryAddWithoutValidation(header.Key.ToString(), commitmes);
+						headerstr = headerstr + header.Key.ToString() + ": " + win1251Bytes + "\n";
 					}
+					else
+					{
+						client.DefaultRequestHeaders.TryAddWithoutValidation(header.Key.ToString(), HttpUtility.HtmlEncode(header.Value)); 
+					}*/
+					//var utf8bytes = Encoding.UTF8.GetBytes(header.Value);
+					//string MessageSignatureValue = Encoding.ASCII.GetString(utf8bytes);
+					//client.DefaultRequestHeaders.TryAddWithoutValidation(header.Key.ToString(), MessageSignatureValue);
+						var header1 = header.Value.ToString();
+						client.DefaultRequestHeaders.TryAddWithoutValidation(header.Key.ToString(), HttpUtility.HtmlEncode(header1));
+						headerstr = headerstr + header.Key.ToString() + ": " + HttpUtility.HtmlEncode(header1) + "\n";
 				}
-			HttpContent content = new StringContent(body, Encoding.UTF8);
+				//client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", @"text/html; charset=windows-1251");
+
+				HttpContent content = new StringContent(body, Encoding.UTF8);
 			httpRequestMessage.Content = content;
 
 			var response = await client.PostAsync(Urllocalgit, content);
@@ -88,9 +113,47 @@ namespace ProxyRest.Controllers
 			}
 			catch (Exception ex)
 			{
-				return "Ошибка обработки запроса: " + ex.Message;
+				return "Ошибка обработки запроса: " + ex.Message  + "\n"+ headerstr;
 			}
 
 		}
+		public string Encoding1251(string codein)
+		{
+			string codeout;
+			Encoding utf8 = Encoding.GetEncoding("UTF-8");
+			Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+
+			Encoding ascii = Encoding.ASCII;
+			Encoding unicode = Encoding.Unicode;
+
+			byte[] unicodeBytes = unicode.GetBytes(codein);
+
+			// Perform the conversion from one encoding to the other.
+			byte[] asciiBytes = Encoding.Convert(unicode, ascii, unicodeBytes);
+
+			char[] asciiChars = new char[ascii.GetCharCount(asciiBytes, 0, asciiBytes.Length)];
+			ascii.GetChars(asciiBytes, 0, asciiBytes.Length, asciiChars, 0);
+			string asciiString = new string(asciiChars);
+
+
+
+			//byte[] win1251Bytes = win1251.GetBytes(codein);
+			//byte[] utf8Bytes = Encoding.Convert(utf8, win1251, win1251Bytes);
+
+			//codeout = win1251.GetString(utf8Bytes);
+			return asciiString;
+
+		}
+		/*
+		public string StringToAscii(string codein)
+		{
+			string codeout = "";
+			foreach (char c in codein)
+			{
+				codeout = codeout + Convert.ToInt32(c);
+			}
+			return codeout;
+		}*/
+
 	}
 }
